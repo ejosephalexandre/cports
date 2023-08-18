7117476bf9f3bfdb7e46cc8d6e83edb8492ac825
Erjo <erjo@cocoba.work>
2023-08-18 17:08:44 +0200

    add makedepends

--- contrib/haproxy/template.py
+++ contrib/haproxy/template.py
@@ -4,6 +4,7 @@ pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
 ]
+hostmakedepends = ["linux-headers"]
 pkgdesc = "High Performance TCP/HTTP Load Balancer"
 maintainer = "Erjo <erjo@cocoba.work>"
 license = "GPL-2.0-or-later"
