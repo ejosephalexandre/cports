ef4133a2949b76e2d8b2bb85e43998e88112f656
Erjo <erjo@cocoba.work>
2023-08-18 15:29:36 +0200

    add HAProxy package

---
+++ contrib/haproxy/template.py
@@ -0,0 +1,12 @@
+pkgname = "haproxy"
+pkgver = "2.8.2"
+pkgrel = 0
+build_style = "gnu_configure"
+configure_args = [
+]
+pkgdesc = "High Performance TCP/HTTP Load Balancer"
+maintainer = "Erjo <erjo@cocoba.work>"
+license = "GPL-2.0-or-later"
+url = "https://www.haproxy.org"
+source = f"https://www.haproxy.org/download/2.8/src/{pkgname}-{pkgver}.tar.gz"
+sha256 = "698d6906d170946a869769964e57816ba3da3adf61ff75e89972b137f4658db0"
