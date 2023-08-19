25d132fc51b160a1179be1e4a59fc19b599ad96b
Erjo <erjo@cocoba.work>
2023-08-20 01:47:20 +0200

    Add suid_files directive and copy license file

--- contrib/sudo/template.py
+++ contrib/sudo/template.py
@@ -14,9 +14,11 @@ pkgdesc = "Allows a system administrator to delegate authority"
 maintainer = "Erjo <erjo@cocoba.work>"
 license = "ISC"
 url = "https://www.sudo.ws"
-source = f"https://www.sudo.ws/dist/{pkgname}-{pkgver}.tar.gz"
+source = f"{url}/dist/{pkgname}-{pkgver}.tar.gz"
 sha256 = "a08318b1c4bc8582c004d4cd9ae2903abc549e7e46ba815e41fe81d1c0782b62"
 
 configure_gen = []
+suid_files = ["sudo"]
 
-
+def post_install(self):
+    self.install_license("LICENSE.md")
