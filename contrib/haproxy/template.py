563b229edf417ad7a9fadcea6138efee713726c5
Erjo <erjo@cocoba.work>
2023-08-22 22:12:41 +0200

    Fix LD

--- contrib/haproxy/template.py
+++ contrib/haproxy/template.py
@@ -1,14 +1,20 @@
 pkgname = "haproxy"
 pkgver = "2.8.2"
 pkgrel = 0
-build_style = "gnu_configure"
-configure_args = [
+build_style = "makefile"
+make_cmd = "gmake"
+make_dir = "."
+make_build_args = [
+    "TARGET=linux-musl",
+    "USE_OPENSSL=1",
+    "USE_LUA=1",
+    "USE_PCRE2=1"
 ]
 hostmakedepends = [
     "automake",
     "linux-headers",
     "openssl-devel",
-    "zlib-devel"
+    "zlib-devel",
 ]
 pkgdesc = "High Performance TCP/HTTP Load Balancer"
 maintainer = "Erjo <erjo@cocoba.work>"
@@ -16,3 +22,7 @@ license = "GPL-2.0-or-later"
 url = "https://www.haproxy.org"
 source = f"{url}/download/2.8/src/{pkgname}-{pkgver}.tar.gz"
 sha256 = "698d6906d170946a869769964e57816ba3da3adf61ff75e89972b137f4658db0"
+
+def init_build(self):
+    self.tool_flags["LD"] = self.tool_flags["CC"]
+
